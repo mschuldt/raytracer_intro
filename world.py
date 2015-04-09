3ce05dd0b672cd7b4d825264f94c85143f505a61
mschuldt <mbschuldt@gmail.com>
2015-04-09 15:44:20 -0700

    extra sphere for shadows

--- world.py
+++ world.py
@@ -9,11 +9,15 @@ reflection_depth = 3
 
 spheres = [make_sphere(1, pos(0, 1,  0), color(1, 0, 0), 16, 0.5),
            make_sphere(1, pos(1, -1, 0), color(0, 1, 0), 64, 0.5),
-           make_sphere(1, pos(-1, -1, 0), color(0, 0, 1), 128, 0.5)]
+           make_sphere(1, pos(-1, -1, 0), color(0, 0, 1), 128, 0.5),
+           make_sphere(0.5, pos(-0.5, 0, 1.5), color(1, 0, 1), 128, 0.5),
+           ]
 
-lights = [light(0.8, pos(2, 0.5, 3))]
+lights = [light(0.4, pos(-2, 0.5, 3))
+          #light(0.8, pos(2, 0.5, 3)),
+]
 
-ambient_light = 0.0
+ambient_light = 0.1
 
 
 ################################################################################
